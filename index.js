d33615a15284d1311a9537879765ceb979abd697
Zach Willis <zachmw27@gmail.com>
2023-10-24 16:43:27 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Zach';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage =  welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
